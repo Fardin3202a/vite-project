66ee9517e83d08c4df4e82f1a22f68231164a46c
Fardin3202a <fardin3202a@gmail.com>
2024-11-29 21:48:32 +0600

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -10,8 +10,8 @@ const Main = () => {
   return (
     <div>
         <div className=' w-screen h-screen flex justify-center items-center flex-col'>
-            <img className=' opacity-90 -z-20 max-w-screen max-h-screen absolute top-12' src="../assets/Mainbg.png" alt="" />
-            <img className=' -z-10 max-w-[1020px] max-h-[650px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src="./src/assets/Vector.png" alt="" />
+            <img className=' opacity-90 -z-20 max-w-screen max-h-screen absolute top-12' src="/VITE-PROJECT/src/assets/Mainbg.PNG" alt="" />
+            <img className=' -z-10 max-w-[1020px] max-h-[650px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src="../src/assets/Vector.png" alt="" />
             <h1 className=' font-poppins font-semibold text-7xl text-transparent bg-clip-text bg-gradient-to-tl from-Secondary to-Primary'>Fardin Ahmed</h1>
             <h1 className=' font-poppins font-semibold text-5xl text-gray-800 leading-tight'>Turning ideas into impactful digital experiences</h1>
             <p className=' w-[960px] h-[136px] font-roboto text-text text-lg text-center leading-tight'>I'm here to help you build modern, responsive, and user-friendly web solutions using HTML, CSS, JavaScript, and React. With a strong focus on design and functionality, I use tools like Tailwind CSS to bring your ideas to life.</p>
